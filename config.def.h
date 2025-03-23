2959f5e2b562a34d3f4d8470012fb2d86b7ae27d
anupamroy777 <github.769@gmail.com>
2025-03-23 22:03:28 +0530

    config: add rule for music player
    Signed-off-by: anupamroy777 <github.769@gmail.com>

--- config.def.h
+++ config.def.h
@@ -43,6 +43,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "strawberry",  NULL,    "MP",       1 << 4,       0,           -1 },
 };
 
 /* layout(s) */
